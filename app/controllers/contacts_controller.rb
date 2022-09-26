0f2cee8a65865de7766516ee1fd5f1f096c01ad1
Will <swilson.carlson@hotmail.com>
2022-09-26 12:46:20 -0400

    update contact working on back end, about to build front end

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,22 +20,58 @@ class ContactsController < ApplicationController
         
     end
 
+    # def update
+    #     contact = Contact.find_by(id: params[:id])
+    #     if contact
+    #         contact.assign_attributes(
+    #             name: params[:name],
+    #             phone: params[:phone],
+    #             email: params[:email],
+    #             image_url: params[:image_url],
+    #             company_name: params[:company_name],
+    #             position: params[:position],
+    #             bio: params[:bio]
+    #         )
+
+    #         contact.company_id = Company.find_or_create_by(name: contact.company_name, user_id: User.first.id).id
+
+    #         contact.save
+    #         render json: contact, status: 204
+    #     else
+    #         render json: {error: "Contact not found"}, status: 422
+    #     end
+    # end
+
+
     def update
         contact = Contact.find_by(id: params[:id])
         if contact
-            contact.assign_attributes(
-                name: params[:name],
-                phone: params[:phone],
-                email: params[:email],
-                image_url: params[:image_url],
-                company_name: params[:company_name],
-                position: params[:position],
-                bio: params[:bio]
-            )
+            company = Company.find_or_create_by(name: params[:company_name], user_id: contact.user_id)
 
-            contact.company_id = Company.find_or_create_by(name: contact.company_name, user_id: User.first.id).id
-            
-            contact.save
+            # params[:name] ? contact.update(name: params[:name]) | contact.update(name: contact.name)
+            if params[:name]
+                contact.update(name: params[:name])
+            end
+
+            if params[:phone]
+                contact.update(phone: params[:phone])
+            end
+            if params[:email]
+                contact.update(email: params[:email])
+            end
+            if params[:image_url]
+                contact.update(image_url: params[:image_url])
+            end
+            if params[:company_name]
+                contact.update(company_name: params[:company_name])
+            end
+            if params[:position]
+                contact.update(position: params[:position])
+            end
+            if params[:bio]
+                contact.update(bio: params[:bio])
+            end
+
             render json: contact, status: 204
         else
             render json: {error: "Contact not found"}, status: 422
--- client/src/Components/Contact/EditContact.js
+++ client/src/Components/Contact/EditContact.js
@@ -1,10 +1,29 @@
-const EditContact = ({id, contact}) => {
+const EditContact = ({id, contact, fetchContact}) => {
     console.log('id', id)
     console.log('contact', contact)
+
+    const updateName = async (e) => {
+        e.preventDefault()
+        console.log('e.target', e.target)
+        console.log('e.target[0]', e.target[0])
+        console.log('e.target[0].value', e.target[0].value)
+        let newName = e.target[0].value
+        let req = await fetch(`http://localhost:3000/contacts/${id}`, {
+            method: "PATCH",
+            body: JSON.stringify({
+                name: newName
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        fetchContact()
+    }
     return(
         <div>
-            <form>
+            <form onSubmit={updateName}>
                 <input type="text" placeholder="Update Name"/>
+                <input type="submit" value="Update Name" />
             </form>
         </div>
     )
