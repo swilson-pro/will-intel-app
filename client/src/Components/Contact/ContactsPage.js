827e8c7e394558fe18fb52bf73a09c4d487c89ee
Will <swilson.carlson@hotmail.com>
2022-09-27 21:07:50 -0400

    got paginate working but no table or anything

--- client/src/Components/Contact/ContactsPage.js
+++ client/src/Components/Contact/ContactsPage.js
@@ -2,204 +2,42 @@ import { useState, useEffect } from "react"
 import { NavLink } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import PaginateContacts from "./PaginateContacts"
+import './paginate.css'
+
 
 const ContactsPage = ({conBlackList}) => {
 
-    // const blackList = ['bio', 'twitter_url', 'image_url', 'user_id']
-
-    let navigate = useNavigate()
-
-    const [ownersNames, setOwnersNames] = useState([])
-    const [owner, setOwner] = useState("All")
-
-    const [owners, setOwners] = useState([])
-    const [paginatedOwner, setPaginatedOwner] = useState("All")
-
     const [contacts, setContacts] = useState([])
 
-    const [keyArray, setKeyArray] = useState([])
-
     const [page, setPage] = useState(1)
-    const [pageCount, setPageCount] = useState()
 
-
-
-    const fetchContacts = async () => {
-        const res = await fetch(`http://localhost:3000/contacts_paginated/${page}`)
-        const contactsPaginatedObject = await res.json()
-        console.log('contactsPaginatedObject', contactsPaginatedObject)
-        const contactsPageDataArray = contactsPaginatedObject.contacts
-        console.log('contactsPageDataArray', contactsPageDataArray)
-        const pagina = contactsPaginatedObject.page
-        console.log('pagina', pagina)
-        const paginaCuenta = contactsPaginatedObject.page_count
-        console.log('paginaCuenta', paginaCuenta)
-        const contactsArray = contactsPageDataArray
-
-        let objKeys = Object.keys(contactsArray[0])
-
-        let displayKeys = objKeys.filter((item) => !conBlackList.includes(item))
-
-        contactsArray.map(objectElement => {
-            conBlackList.map((element) => delete objectElement[element])
-        })
-
-        setKeyArray(displayKeys)
-
-        setContacts(contactsArray)
-
-    }
-
-    const updateOwner = async (e) => {
-
-        console.log('owners', owners)
-        e.preventDefault()
-
-        console.log('e', e)
-        console.log('e.target', e.target)
-        console.log('e.target[0]', e.target[0])
-        console.log('e.target[0].value', e.target[0].value)
-
-        const result = owners.find(owner => {
-            return owner.name == e.target[0].value;
-        })
-
-        console.log('result', result)
-
-        let res = await fetch(`http://localhost:3000/owners_contacts_paginated/${result.id}/${page}`)
-        const contactsPaginatedObject = await res.json()
-        const contactsArray = contactsPaginatedObject.contacts
-        console.log('contactsArray', contactsArray)
-        setContacts(contactsArray)
-
-    
-    }
-
-
-    const fetchOwnersNames = async () => {
-        const response = await fetch(`http://localhost:3000/users_names`)
-        const ownersNamesArray = await response.json()
-        const res = await fetch(`http://localhost:3000/users`)
-        const ownersArray = await res.json()
-        setOwners(ownersArray)
-        setOwnersNames(ownersNamesArray)
-    }
+    const [pageCount, setPageCount] = useState();
 
     useEffect(() => {
-        // fetchContacts()
-        fetchOwnersNames()
-        fetchContacts()
-    }, [paginatedOwner, owner])
+        getContactsForPage(page)
+    }, [])
 
-    let formatter = (str) => {
-        let arr = str.split('')
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] == '_') {
-                arr[i] = " "
-            }
-        }
-        for (let i = 0; i < arr.length; i++) {
-            if (i == 0 || arr[i - 1] == " ") {
-                arr[i] = arr[i].toUpperCase()
-            }
-        }
-        let result = ""
-        for (let i = 0; i < arr.length; i++) {
-            result = result + arr[i]
-        }
-        return result
+    const getContactsForPage = async (page) => {
+        console.log('page', page)
+        const res = await fetch(`http://localhost:3000/contacts_paginated/${page}`)
+        const contactsPageData = await res.json()
+        console.log('contactsPageData', contactsPageData)
+        const contactsDataArray = contactsPageData.contacts
+        console.log('contactsDataArray', contactsDataArray)
+        const pagina = contactsPageData.page
+        console.log(pagina)
+        const paginaCuenta = contactsPageData.page_count
+        setPageCount(paginaCuenta)
+        console.log('paginaCuenta', paginaCuenta)
+        setContacts(contactsDataArray)
     }
 
-    const handleClick = (id) => {
-
-        navigate(`/contacts/${id}`)
-    }
-
-
+    console.log('contacts', contacts)
 
     return(
         <main>
-            <div className='top-of-page'>
-            <NavLink className='new-contact-navlink' to='/contacts/new' >
 
-                <button className="new-contact">Create Contact</button></NavLink>
-
-{/* PAGINATED FILTERS */}
-
-            <div>
-                <form onSubmit={updateOwner}>
-                    <label>PAGINATE Choose Owner:</label>
-                    <select name='ownersNames' id='ownersNames'>
-                        <option value="All">All</option>
-                        {ownersNames.map((ownerName) => {
-                           return <option key={ownerName} value={ownerName}>{ownerName}</option>
-                        })}
-                    </select>
-                    <input type="submit" value="Update Owner" />
-                </form>
-            </div>
-
-            <div className="filter">
-
-                <h4>Total: {contacts.length}</h4>
-            </div>
-            <PaginateContacts pageCount={pageCount} setPageCount={setPageCount}/>
-            </div>
-            <div className="fixTableHead">
-            <table className='contacts-table'>
-                <thead>
-                    <tr>
-                        {keyArray.map((key) => {
-                            return(
-                                <th key={key}>{formatter(key)}</th>
-                            )
-                        })}
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    {contacts.map(contact=>{
-                        // console.log('Object.keys(contact)', Object.keys(contact))
-                        let contactKeys = Object.keys(contact)
-                        // console.log('contactKeys', contactKeys)
-                        let contactVals = Object.values(contact)
-                        // console.log('contactVals', contactVals)
-
-                        return (
-                            <tr key={contact.id} onClick={() => handleClick(contact.id)}>
-                        {contactVals.map((val, index) => {
-
-                            // console.log('val', val)
-                            return (
-                                <td key={index}>{val}</td>
-                            )
-                        })}
-                        </tr>)
-                        // return (
-                            // <tr>
-                            //     <td>{contact.id}</td>
-                            //     <td>{contact.name}</td>
-                            //     <td>{contact.company_name}</td>
-                            //     <td>{contact.position}</td>
-                            //     <td>{contact.bio}</td>
-                            //     <td>{contact.phone}</td>
-                            //     <td>{contact.email}</td>
-                            //     <td>{contact.location}</td>
-                            //     <td>{contact.twitter_url}</td>
-                            //     <td>{contact.linkedin_profile_url}</td>
-                            //     <td>{contact.linkedin_company_url}</td>
-                            //     <td>{contact.user_id}</td>
-                            //     <td>{contact.image_url}</td>
-
-                            // </tr>
-                        // )
-                    })}
-                    
-                </tbody>
-            </table>
-            </div>
-
-
+            <PaginateContacts pageCount={pageCount} setPageCount={setPageCount} getContactsForPage={getContactsForPage} data={contacts}/>
 
         </main>
     )
--- client/src/Components/Contact/PaginateContacts.js
+++ client/src/Components/Contact/PaginateContacts.js
@@ -1,6 +1,56 @@
-const PaginateContacts = () => {
+import { useEffect, useState } from "react"
+import ReactPaginate from 'react-paginate'
+
+const PaginateContacts = ({pageCount, setPageCount, data, getContactsForPage}) => {
+
+    console.log('pageCount', pageCount)
+
+    const [currentItems, setCurrentItems] = useState([]);
+
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 10;
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+
+        setCurrentItems(data.slice(itemOffset, endOffset));
+    }, [itemOffset, itemsPerPage, data]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.length;
+        console.log(
+            `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+
+        getContactsForPage(event.selected+1)
+    }
     return (
-        <div>REACT PAGINATE</div>
+        <>
+        <div>
+            {currentItems.map(contact => {
+                return (
+                    <div>
+                        <h3>{contact.name}</h3>
+                    </div>
+                )
+            })}
+        </div>
+        <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            pageCount={pageCount}
+            previousLabel="< previous"
+            renderOnZeroPageCount={null}
+            containerClassName="pagination"
+            pageLinkClassName="page-num"
+            previousLinkClassName="page-num"
+            nextLinkClassName="page-num"
+            activeLinkClassName="active"
+        />
+        </>
     )
 }
 
