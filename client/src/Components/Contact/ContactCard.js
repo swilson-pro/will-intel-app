eb02c363236ffc42099f3a0a1c881dea507763b3
Will <swilson.carlson@hotmail.com>
2022-09-29 11:01:15 -0400

    commented out all fonts, then added M ontserrat to index.css

--- client/src/Components/Contact/ContactCard.js
+++ client/src/Components/Contact/ContactCard.js
@@ -2,7 +2,7 @@ import { Navigate, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import EditContact from "./EditContact"
-import './ContactCard.css'
+// import './ContactCard.css'
 
 const ContactCard = () => {
 
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,30 +1,52 @@
 import { Link } from 'react-router-dom'
-import styles from './Style/NavBar.css'
+import './Style/NavBar.css'
 
 const NavBar = () => {
     return (
-        <div className='navbar'>
-            <ul className='nav-links'>
-                <li className='nav-item' id='nav-company-name'>
-                    Carlson
-                </li>
-                <li className='nav-item'>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/contacts'>Contacts</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/companies'>Companies</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/products'>Products</Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/initiatives'>Initiatives</Link>
-                </li>
-            </ul>
+        <div className='header'>
+            <div className='header_left'>
+                <h1>Carlson</h1>
+            </div>
+            <div className='header-right'>
+                <div className='headerOption'>
+                    <i className='material-icons headerOption_icon'></i>
+                    <h3>Contacts</h3>
+                </div>
+                <div className='header-option'>
+                    <i className='material-icons headerOption_icon'></i>
+                    <h3>Companies</h3>
+                </div>
+                <div className='header-option'>
+                    <i className='material-icons headerOption_icon'></i>
+                    <h3>Products</h3>
+                </div>
+            </div>
+
         </div>
+
+
+        // <div className='navbar'>
+        //     <ul className='nav-links'>
+        //         <li className='nav-item' id='nav-company-name'>
+        //             Carlson
+        //         </li>
+        //         <li className='nav-item'>
+        //             <Link to='/'>Home</Link>
+        //         </li>
+        //         <li className='nav-item'>
+        //             <Link to='/contacts'>Contacts</Link>
+        //         </li>
+        //         <li className='nav-item'>
+        //             <Link to='/companies'>Companies</Link>
+        //         </li>
+        //         <li className='nav-item'>
+        //             <Link to='/products'>Products</Link>
+        //         </li>
+        //         <li className='nav-item'>
+        //             <Link to='/initiatives'>Initiatives</Link>
+        //         </li>
+        //     </ul>
+        // </div>
     )
 }
 
