b1c0ab4fb7b71cd1fde5c507313660d9d266a018
Will <swilson.carlson@hotmail.com>
2022-09-29 12:57:48 -0400

    connected navbar links to pages

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -21,26 +21,35 @@ const NavBar = () => {
                 </div>
             </div>
             <div className='header_right'>
-                <div className='headerOption'>
-                    {/* <i className='material-icons headerOption_icon'></i> */}
-                    <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faHome}></FontAwesomeIcon>
-                    <h3>Home</h3>
-                </div>
-                <div className='headerOption'>
-                    {/* <i className='material-icons headerOption_icon'></i> */}
-                    <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faUser}></FontAwesomeIcon>
-                    <h3>Contacts</h3>
-                </div>
-                <div className='headerOption'>
-                    {/* <i className='material-icons headerOption_icon'></i> */}
-                    <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faBuilding}></FontAwesomeIcon>
-                    <h3>Companies</h3>
-                </div>
-                <div className='headerOption'>
-                    {/* <i className='material-icons headerOption_icon'></i> */}
-                    <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faBusinessTime}></FontAwesomeIcon>
-                    <h3>Products</h3>
-                </div>
+                <Link className='link' to='/'>
+                    <div className='headerOption'>
+                        {/* <i className='material-icons headerOption_icon'></i> */}
+                        
+                        <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faHome}></FontAwesomeIcon>
+                        <h3>Home</h3>
+                    </div>
+                </Link>
+                <Link className='link' to='/contacts'>
+                    <div className='headerOption'>
+                        {/* <i className='material-icons headerOption_icon'></i> */}
+                        <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faUser}></FontAwesomeIcon>
+                        <h3>Contacts</h3>
+                    </div>
+                </Link>
+                <Link className='link' to='/companies'>
+                    <div className='headerOption'>
+                        {/* <i className='material-icons headerOption_icon'></i> */}
+                        <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faBuilding}></FontAwesomeIcon>
+                        <h3>Companies</h3>
+                    </div>
+                </Link>
+                <Link className='link' to='/products'>
+                    <div className='headerOption'>
+                        {/* <i className='material-icons headerOption_icon'></i> */}
+                        <FontAwesomeIcon className='materials-icons headerOption_icon' icon={faBusinessTime}></FontAwesomeIcon>
+                        <h3>Products</h3>
+                    </div>
+                </Link>
             </div>
             
 
