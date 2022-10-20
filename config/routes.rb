40b449a985d127b3dd9048333750f7776f3fe599
Will <swilson.carlson@hotmail.com>
2022-10-20 12:37:53 -0400

    added get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

--- config/routes.rb
+++ config/routes.rb
@@ -74,5 +74,6 @@ Rails.application.routes.draw do
     # post "/login", to: "sessions#create"
     # delete "/logout", to: "sessions#destroy"
     get '/chickens', to: 'chickens#index'
+    get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   end
 end
